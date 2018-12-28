b75a0581e5d656603df8bc523060c23f4348b7fd
bravedevrabbit <williamsmoisha@gmail.com>
2018-12-28 16:03:03 +0500

    + electron

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer  = electron.ipcRenderer;
+
 class App extends Component {
   render() {
     return (
