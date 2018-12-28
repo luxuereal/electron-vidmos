602b4de455c5ff59ad8ba8ce04a4ae42731dc6e3
bravedevrabbit <williamsmoisha@gmail.com>
2018-12-28 23:33:16 +0500

    + ipcMain

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 
 let mainWindow;
 let createWindow = () => {
     mainWindow = new BrowserWindow({
         show: false,
+        frame: (process.platform !== 'darwin') ? false : true,
+        titleBarStyle: 'hidden'
     });
     mainWindow.loadURL('http://localhost:3000');
     mainWindow.on('closed', () => mainWindow = null);
     mainWindow.once('ready-to-show', () => mainWindow.show());
-};
 
+     
+    mainWindow.webContents.send('is-maximized', mainWindow.isMaximized());
+    mainWindow.on('maximize', () => mainWindow.webContents.send('is-maximized', mainWindow.isMaximized()));
+    mainWindow.on('unmaximize', () => mainWindow.webContents.send('is-maximized', mainWindow.isMaximized()));
+};
 
 app.on('ready', createWindow);
 
 app.on('activate', () => {
     if(mainWindow === null) createWindow();
 });
-
 app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit();
+});
+
+ipcMain.on('minimize', () => {
+    mainWindow.minimize();
+});
+
+ipcMain.on('maximize', () => {
+    if(mainWindow.isMaximized()){
+        mainWindow.unmaximize();
+    }else{
+        mainWindow.maximize();
+    }
+});
+
+ipcMain.on('exit', () => {
     if(process.platform !== 'darwin') app.quit();
 });
