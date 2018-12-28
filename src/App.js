48da801a8c05ab78b8ea7ae2b613e0d77264d8cd
bravedevrabbit <williamsmoisha@gmail.com>
2018-12-28 23:33:30 +0500

    + ipc Renderer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Titlebar from './Components/Titlebar';
 import './App.css';
 
 const electron = window.require('electron');
@@ -7,11 +7,48 @@ const fs = electron.remote.require('fs');
 const ipcRenderer  = electron.ipcRenderer;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "Vidmos",
+      isMax: false,
+    }
+    ipcRenderer.on('is-maximized', (event, bool) => {
+      this.setState({isMax: bool});
+    });
+  }
+  componentWillMount(){
+  }
+  prevVideo() {
+  }
+  nextVideo() {
+  }
+  playPauseVideo() {
+  }
+  closeApp() {
+    ipcRenderer.send('exit');
+  }
+  maximizeApp(event) {
+    ipcRenderer.send('maximize');
+  }
+  minimizeApp() {
+    ipcRenderer.send('minimize');
+  }
   render() {
     return (
       <div className="App">
+        <Titlebar 
+          title={this.state.title}
+          isMax={this.state.isMax}
+          prevVideo={this.prevVideo.bind(this)}
+          nextVideo={this.nextVideo.bind(this)}
+          playPauseVideo={this.playPauseVideo.bind(this)}
+          closeApp={this.closeApp.bind(this)}
+          maximizeApp={this.maximizeApp.bind(this)}
+          minimizeApp={this.minimizeApp.bind(this)}
+        />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
