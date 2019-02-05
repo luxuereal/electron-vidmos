373653991d3b48dbe9db5fed76e5155936e2bd02
bravedevrabbit <williamsmoisha@gmail.com>
2019-02-05 19:21:53 +0500

    Handles file opening from Process #4

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 const electron = require('electron');
 const {app, BrowserWindow, ipcMain, dialog, globalShortcut} = electron;
 
 let mainWindow;
+let isDialogOpened = false;
+
 let createWindow = () => {
     mainWindow = new BrowserWindow({
         minHeight: 600,
         minWidth: 800,
         show: false,
+        icon: path.resolve(__dirname, './app.icon.png'),
         frame: (process.platform !== 'darwin') ? false : true,
         titleBarStyle: 'hidden',
         webPreferences: {
@@ -36,29 +40,49 @@ let createWindow = () => {
         globalShortcut.register('CommandOrControl+Right', () => mainWindow.webContents.send('seekNext'));
         globalShortcut.register('CommandOrControl+Shift+I', () => mainWindow.webContents.toggleDevTools());
     });
-    mainWindow.on('blur', () => globalShortcut.unregisterAll());    
+    processHandler();
+    mainWindow.on('blur', () => globalShortcut.unregisterAll());  
+};
+
+let processHandler = () => {
+    let ext;
+    let arr = [];
+    for (let i = 0; i < process.argv.length; i++) {
+        ext = process.argv[i].split('.').pop();
+        if(ext === 'mp4' || ext === 'webm' || ext === 'ogg' || ext === 'mkv'){
+            arr.push(process.argv[i]);
+        }
+    }  
+    mainWindow.webContents.send('playables', arr);
+    console.log(arr);
 };
 
 let openFiles = (event, type) => {
-    dialog.showOpenDialog({
-        title: 'Open Files',
-        properties: (type === 'file') ? ['openFile', 'multiSelections'] : ['openDirectory'],
-        filters: [{ name: 'Video Files', extensions: ['mp4', 'webm', 'ogg', 'mkv'] }]
-    }, (files) => {
-        if (files !== undefined) {
-            if (type === 'file') {
-                mainWindow.webContents.send('playables', files);
-            } else {
-                let arr = [];
-                let dir = fs.readdirSync(files[0]);
-                for (let i = 0; i < dir.length; i++) {
-                    if (dir[i] !== files[0])
-                        arr.push(files[0] + '\\' + dir[i]);
+    if(!isDialogOpened){
+        isDialogOpened = true;
+        dialog.showOpenDialog({
+            title: (type === 'file') ? 'Open File(s)' : 'Open Directory',
+            properties: (type === 'file') ? ['openFile', 'multiSelections'] : ['openDirectory'],
+            filters: [{ name: 'Video Files', extensions: ['mp4', 'webm', 'ogg', 'mkv'] }]
+        }, (files) => {
+            if (files !== undefined) {
+                if (type === 'file') {
+                    mainWindow.webContents.send('playables', files);
+                } else {
+                    let fileExt;
+                    let arr = [];
+                    let dir = fs.readdirSync(files[0]);
+                    for (let i = 0; i < dir.length; i++) {
+                        fileExt = dir[i].split('.').pop();
+                        if (dir[i] !== files[0] && (fileExt === 'mp4' || fileExt === 'webm' || fileExt === 'ogg' || fileExt === 'mkv'))
+                            arr.push(files[0] + '\\' + dir[i]);
+                    }
+                    mainWindow.webContents.send('playables', arr);
                 }
-                mainWindow.webContents.send('playables', arr);
             }
-        }
-    });
+            isDialogOpened = false;
+       });
+    }
 };
 
 app.on('ready', createWindow);
